64395dded38afb7844b0288c2c8cc43584dfb062
rhastaea <shalomilan@gmail.com>
2023-02-10 15:57:57 -0300

    Modificaciones a algunos archivos

--- Estaciones_Watson.R
+++ Estaciones_Watson.R
@@ -33,6 +33,26 @@ datacam_cingulata <- datacam %>% filter(Species == "cingulata")
 datacam_mazama <- datacam %>% filter(Species == "mazama")
 datacam_leopardus <- datacam %>% filter(Species == "leopardus")
 
+################################### MERDIA - WATSON - WHEELER ####
+wwh_axis <- watson.wheeler.test(datacam_axis$r.Time ~ datacam_axis$Estacion, data = datacam_axis)
+wwh_hydrochoerus <- watson.wheeler.test(datacam_hydrochoerus$r.Time ~ datacam_hydrochoerus$Estacion, data = datacam_hydrochoerus)
+wwh_canidae <- watson.wheeler.test(datacam_canidae$r.Time ~ datacam_canidae$Estacion, data = datacam_canidae)
+wwh_sus <- watson.wheeler.test(datacam_sus$r.Time ~ datacam_sus$Estacion, data = datacam_sus)
+wwh_rhea <- watson.wheeler.test(datacam_rhea$r.Time ~ datacam_rhea$Estacion, data = datacam_rhea)
+wwh_cingulata <- watson.wheeler.test(datacam_cingulata$r.Time ~ datacam_cingulata$Estacion, data = datacam_cingulata)
+wwh_mazama <- watson.wheeler.test(datacam_mazama$r.Time ~ datacam_mazama$Estacion, data = datacam_mazama)
+wwh_leopardus <- watson.wheeler.test(datacam_leopardus$r.Time ~ datacam_leopardus$Estacion, data = datacam_leopardus)
+
+Taxon <- c("Ciervo axis","Carpincho","Zorros","Jabalí","Ñandú","Mulitas","Corzuela parda","Gato montés")
+wwh <- c(wwh_axis[3],wwh_hydrochoerus[3],wwh_canidae[3],wwh_sus[3],wwh_rhea[3],wwh_cingulata[3],wwh_mazama[3],wwh_leopardus[3])
+pvalor_wwh <- c(wwh_axis[4],wwh_hydrochoerus[4],wwh_canidae[4],wwh_sus[4],wwh_rhea[4],wwh_cingulata[4],wwh_mazama[4],wwh_leopardus[4])
+GL_wwh <- c(wwh_axis[2],wwh_hydrochoerus[2],wwh_canidae[2],wwh_sus[2],wwh_rhea[2],wwh_cingulata[2],wwh_mazama[2],wwh_leopardus[2])
+
+tabla_wwh <- data.frame(unlist(Taxon),round(unlist(wwh),2),round(unlist(pvalor_wwh),3),unlist(GL_wwh))
+colnames(tabla_wwh) <- c("Taxón", "Watson-Wheeler", "p-valor","GL")
+tabla_wwh
+#########################3
+
 ##################### WATSON - WILLIAMS ####
 
 wwi_axis <- watson.williams.test(datacam_axis$r.Time ~ datacam_axis$Estacion, data = datacam_axis)
@@ -52,23 +72,3 @@ GL_wwi <- c(wwi_axis[2],wwi_hydrochoerus[2],wwi_canidae[2],wwi_sus[2],wwi_rhea[2
 tabla_wwi <- data.frame(unlist(Taxon),unlist(wwi),round(unlist(pvalor_wwi),3),unlist(GL_wwi))
 colnames(tabla_wwi) <- c("Taxón", "Watson-Williams", "p-valor","GL")
 tabla_wwi[1:8,]
-
-################################### MERDIA - WATSON - WHEELER ####
-wwh_axis <- watson.wheeler.test(datacam_axis$r.Time ~ datacam_axis$Estacion, data = datacam_axis)
-wwh_hydrochoerus <- watson.wheeler.test(datacam_hydrochoerus$r.Time ~ datacam_hydrochoerus$Estacion, data = datacam_hydrochoerus)
-wwh_canidae <- watson.wheeler.test(datacam_canidae$r.Time ~ datacam_canidae$Estacion, data = datacam_canidae)
-wwh_sus <- watson.wheeler.test(datacam_sus$r.Time ~ datacam_sus$Estacion, data = datacam_sus)
-wwh_rhea <- watson.wheeler.test(datacam_rhea$r.Time ~ datacam_rhea$Estacion, data = datacam_rhea)
-wwh_cingulata <- watson.wheeler.test(datacam_cingulata$r.Time ~ datacam_cingulata$Estacion, data = datacam_cingulata)
-wwh_mazama <- watson.wheeler.test(datacam_mazama$r.Time ~ datacam_mazama$Estacion, data = datacam_mazama)
-wwh_leopardus <- watson.wheeler.test(datacam_leopardus$r.Time ~ datacam_leopardus$Estacion, data = datacam_leopardus)
-
-Taxon <- c("Ciervo axis","Carpincho","Zorros","Jabalí","Ñandú","Mulitas","Corzuela parda","Gato montés")
-WWh <- c(WWh_axis[3],WWh_hydrochoerus[3],WWh_canidae[3],WWh_sus[3],WWh_rhea[3],WWh_cingulata[3],WWh_mazama[3],WWh_leopardus[3])
-pvalor_WWh <- c(WWh_axis[4],WWh_hydrochoerus[4],WWh_canidae[4],WWh_sus[4],WWh_rhea[4],WWh_cingulata[4],WWh_mazama[4],WWh_leopardus[4])
-GL_WWh <- c(WWh_axis[2],WWh_hydrochoerus[2],WWh_canidae[2],WWh_sus[2],WWh_rhea[2],WWh_cingulata[2],WWh_mazama[2],WWh_leopardus[2])
-
-tabla_WWh <- data.frame(unlist(Taxon),unlist(WWh),round(unlist(pvalor),3),unlist(GL))
-colnames(tabla_WWh) <- c("Taxón", "Watson-Wheeler", "p-valor","GL")
-tabla_WWh
-#########################3
